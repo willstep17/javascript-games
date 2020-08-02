4c139a0044e476327b042ab11073914b1bd28a9e
William Stephens <william.a.stephens17@gmail.com>
2020-08-01 23:03:47 -0500

    Whack a Mole start

---
+++ js/whackAMole.js
@@ -0,0 +1,4 @@
+const square = document.querySelectorAll('.game-square');
+const mole = document.querySelectorAll('.mole');
+const timeLeft = document.querySelector('#time-left');
+let score = document.querySelector('#score');
